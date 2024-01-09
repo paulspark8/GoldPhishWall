e8b54065ca7e1b8b26353588e556ac9fb2334d70
paulspark8 <105112675+paulspark8@users.noreply.github.com>
2024-01-09 14:58:10 +0200

    error, to be fixed

--- backend/lib_b_mail.py
+++ backend/lib_b_mail.py
@@ -1,13 +1,27 @@
-import mail
-
-def str_to_mail():
-    pass
-
-def mail_to_str():
-    pass
-
-def get_mail_field():
-    pass
+import email
+from enum import Enum
+class Logtype(Enum):
+    DEBUG = 0
+    INFO = 1
+    WARNING = 2
+    CRITICAL = 3
+    DEAD = 4
 
 def get_mail_text():
     pass
+
+def create_log_entry(type: Logtype):
+    pass
+
+def delete_log_entry():
+    pass
+
+def send_log_entry():
+    pass
+
+def export_log_entry():
+    pass
+
+def print_log_entry():
+    pass
+
---
+++ backend/test_lib_b_mail.py
@@ -0,0 +1,21 @@
+from lib_b_mail import *
+import unittest 
+
+
+def get_mail_text():
+    pass
+
+def test_create_log_entry(Logtype.DEBUG.value):
+    pass
+
+def test_delete_log_entry():
+    pass
+
+def test_send_log_entry():
+    pass
+
+def test_export_log_entry():
+    pass
+
+def test_print_log_entry():
+    pass
